a34e1e2ced9840dfab44b40288d056ef680eb0af
FerSR <yugi-fer@hotmail.com>
2019-07-31 12:42:13 -0500

    variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -13,7 +13,6 @@ let urlDB;
  if (process.env.NODE_ENV === "dev") {
    urlDB = "mongodb://localhost:27017/Cafe";
  } else {
-  urlDB =
-    "mongodb+srv://root:IOLEiNvCHdOhQ7RW@cluster0-xbuob.mongodb.net/cafe";
+  urlDB =process.env.MONGO_URI
 }
 process.env.URLDB=urlDB
