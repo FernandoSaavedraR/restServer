d2f5316029bf730ae2016ad53f97e94cb696516d
FerSR <yugi-fer@hotmail.com>
2019-07-30 20:56:52 -0500

    Agregando POST,GET,PUT,DELETE al restServer

---
+++ server/routes/usuario.js
@@ -0,0 +1,121 @@
+const express = require("express");
+const app = express();
+const Usuario = require("../models/usuario");
+const bcrypt = require("bcrypt");
+const _ = require("underscore");
+//pedir datos
+app.get("/usuario", function(req, res) {
+  let from = Number(req.query.from) || 0;
+  let limit = Number(req.query.limit) || 5;
+  let condicionesBusqueda ={estado:true}
+  Usuario.find(condicionesBusqueda, "nombre email role estado google img")
+    .skip(from)
+    .limit(limit)
+    .exec((err, usuarios) => {
+      if (err)
+        return res.status(400).json({
+          ok: false,
+          err
+        });
+
+      Usuario.countDocuments(condicionesBusqueda, (err, conteo) => {
+        if (err)
+          return res.status(400).json({
+            ok: false,
+            err
+          });
+        res.json({ ok: true, conteo, usuarios });
+      });
+    });
+});
+
+//mandar datos
+app.post("/usuario", function(req, res) {
+  let body = req.body;
+  let usuario = new Usuario({
+    nombre: body.nombre,
+    email: body.email,
+    password: bcrypt.hashSync(body.password, 10),
+    role: body.role
+  });
+  usuario.save((err, usuarioDB) => {
+    if (err)
+      return res.status(400).json({
+        ok: false,
+        err
+      });
+    //usuarioDB.password= null
+    res.json({ ok: true, usuarioDB });
+  });
+});
+
+//actualizar datos
+app.put("/usuario/:id", function(req, res) {
+  let id = req.params.id;
+  let body = _.pick(req.body, ["nombre", "email", "img", "role", "estado"]);
+  Usuario.findByIdAndUpdate(
+    id,
+    body,
+    { new: true, runValidators: true },
+    (err, usuarioDB) => {
+      if (err)
+        return res.status(400).json({
+          ok: false,
+          err
+        });
+      res.json({ ok: true, usuarioDB });
+    }
+  );
+});
+app.delete("/usuario/:id", function(req, res) {
+  let id = req.params.id;
+  let cambiaEstado ={
+    estado:false
+  }
+  Usuario.findByIdAndUpdate(id,cambiaEstado,{ new: true}, (err, usuarioBorrado) => {
+    if (err)
+    return res.status(400).json({
+        ok: false,
+        err
+      });
+      if(!usuarioBorrado){
+        return res.status(400).json({
+        ok: false,
+        err:{
+          message: 'Usuario no encontrado'
+        }
+      });
+    }
+    res.json({
+      ok:true,
+      usuario:usuarioBorrado
+    })
+  });
+});
+
+module.exports = app;
+
+/*============================funciones extra================================*/
+//"borrar" (cambiar estado) de los datos
+// app.delete("/usuario/:id", function(req, res) {
+//   let id = req.params.id;
+//   Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
+//     if (err)
+//       return res.status(400).json({
+//         ok: false,
+//         err
+//       });
+//     if(!usuarioBorrado){
+//       return res.status(400).json({
+//         ok: false,
+//         err:{
+//           message: 'Usuario no encontrado'
+//         }
+//       });
+//     }
+//       res.json({
+//         ok:true,
+//         usuario:usuarioBorrado
+//       })
+//   });
+// });
--- server/server.js
+++ server/server.js
@@ -1,35 +1,23 @@
 require('./config/config')
+
 const express = require("express");
-const bodyParser = require("body-parser");
 const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose')
+
 //middlewares
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-//pedir datos
-app.get("/usuario", function(req, res) {
-  res.json("get Usuario");
-});
-//mandar datos
-app.post("/usuario", function(req, res) {
-  let body = req.body;
-  if (body.nombre === undefined) {
-      res.status(400).json({
-          ok:false,
-          mensaje:'El nombre es necesario'
-      })
-  } else {
-    res.json({ body, metodo: "post Usuario" });
-  }
-});
-//actualizar datos
-app.put("/usuario/:id", function(req, res) {
-  let id = req.params.id;
-  res.json({ id, metodo: "put usuario" });
-});
-//"borrar" (cambiar estado) de los datos
-app.delete("/usuario", function(req, res) {
-  res.json("delete Usuario");
-});
+app.use(require('./routes/usuario'))
+
+
+
+mongoose.connect(process.env.URLDB,{ useNewUrlParser: true, useFindAndModify: false },
+(err,res)=>{
+  if(err) throw err
+  console.log('Base de datos online');
+})
+mongoose.set('useCreateIndex', true);
 
 app.listen(process.env.PORT, () => {
   console.log(`Escuchando puerto ${process.env.PORT}`);
