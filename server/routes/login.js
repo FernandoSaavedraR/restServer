de518ad96accaf9c96db7789006a6c01c638af5a
FerSR <yugi-fer@hotmail.com>
2019-08-03 17:15:56 -0500

    google Sign-IN

--- server/routes/login.js
+++ server/routes/login.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const Usuario = require("../models/usuario");
 const bcrypt = require("bcrypt");
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
+const { OAuth2Client } = require("google-auth-library");
+const client = new OAuth2Client(process.env.CLIENT_ID);
 app.post("/login", (req, res) => {
   let body = req.body;
-  Usuario.findOne({ email: body.email,estado:true }, (err, usuarioDB) => {
+  Usuario.findOne({ email: body.email, estado: true }, (err, usuarioDB) => {
     if (err)
       return res.status(400).json({
         ok: false,
@@ -27,15 +29,103 @@ app.post("/login", (req, res) => {
         }
       });
     }
-    let token = jwt.sign({
-        usuario:usuarioDB  
-    },process.env.SEED,{expiresIn:process.env.CADUCIDAD_TOKEN})
+    let token = jwt.sign(
+      {
+        usuario: usuarioDB
+      },
+      process.env.SEED,
+      { expiresIn: process.env.CADUCIDAD_TOKEN }
+    );
     res.json({
-        ok:true,
-        usuario:usuarioDB,
-        token
-    })
+      ok: true,
+      usuario: usuarioDB,
+      token
+    });
   });
-  
 });
+
+//google config
+async function verify(token) {
+  const ticket = await client.verifyIdToken({
+    idToken: token,
+    audience: process.env.CLIENT_ID // Specify the CLIENT_ID of the app that accesses the backend
+    // Or, if multiple clients access the backend:
+    //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+  });
+  const payload = ticket.getPayload();
+  //const userid = payload["sub"];
+  // If request specified a G Suite domain:
+  //const domain = payload['hd'];
+  return {
+    nombre: payload.name,
+    email: payload.email,
+    img: payload.picture,
+    google: true
+  };
+}
+app.post("/google", async (req, res) => {
+  let token = req.body.idtoken;
+  let googleUser = await verify(token).catch(err =>
+    res.status(403).json({
+      ok: false,
+      err
+    })
+  );
+  Usuario.findOne({ email: googleUser.email }, (err, usuarioDB) => {
+    if (err)
+      return res.status(500).json({
+        ok: false,
+        err
+      });
+    if (usuarioDB) {
+      if (!usuarioDB.google) {
+        return res.status(400).json({
+          ok: false,
+          err: "Use autenticación normal"
+        });
+      } else {
+        let token = jwt.sign(
+          {
+            usuario: usuarioDB
+          },
+          process.env.SEED,
+          { expiresIn: process.env.CADUCIDAD_TOKEN }
+        );
+        return res.status(200).json({
+          ok: true,
+          usuario: usuarioDB,
+          token
+        });
+      }
+    } else {
+      let usuario = new Usuario();
+      usuario.nombre = googleUser.nombre;
+      usuario.email = googleUser.email;
+      usuario.img = googleUser.picture;
+      usuario.google = true;
+      usuario.password = bcrypt.hashSync("hash de 10 vueltas", 10);
+      usuario.save((err, usuarioDB) => {
+        if (err)
+          return res.status(500).json({
+            ok: false,
+            err
+          });
+        let token = jwt.sign(
+          {
+            usuario: usuarioDB
+          },
+          process.env.SEED,
+          { expiresIn: process.env.CADUCIDAD_TOKEN }
+        );
+        res.json({
+          ok: true,
+          usuario: usuarioDB,
+          token
+        });
+      });
+    }
+  });
+  //res.json({usuario:googleUser});
+});
+
 module.exports = app;
--- server/server.js
+++ server/server.js
@@ -4,13 +4,13 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose')
-
+const path = require('path')
 //middlewares
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(require('./routes/index'))
-
-
+//habilitar carpeta publica
+app.use(express.static(path.resolve(__dirname,'../public')))
 mongoose.connect(process.env.URLDB,{ useNewUrlParser: true, useFindAndModify: false },
 (err,res)=>{
   if(err) throw err
